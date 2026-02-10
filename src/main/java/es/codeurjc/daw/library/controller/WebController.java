15acdf38fd9b010fb52d6940cff57fabdc62fe7a
Javier004 <j.hernandezca.2023@alumnos.urjc.es>
2026-02-10 12:57:22 +0100

    configuracion para que al levantar spring carge la web(index)

---
+++ src/main/java/es/codeurjc/daw/library/controller/WebController.java
@@ -0,0 +1,13 @@
+package es.codeurjc.daw.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "user/index";
+    }
+}
