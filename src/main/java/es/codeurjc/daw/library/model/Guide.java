6f5da17904089290ffe4173353b1adc2dee446e5
Javier004 <j.hernandezca.2023@alumnos.urjc.es>
2026-02-14 11:35:55 +0100

    creacion de la tabla Guide en la BD

---
+++ src/main/java/es/codeurjc/daw/library/model/Guide.java
@@ -0,0 +1,38 @@
+package es.codeurjc.daw.library.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "guides")
+public class Guide {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+
+    private double price;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "tour_id")
+    private Tour tour;
+
+    public Guide() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public Tour getTour() { return tour; }
+    public void setTour(Tour tour) { this.tour = tour; }
+}
---
+++ src/main/java/es/codeurjc/daw/library/service/GuideService.java
@@ -0,0 +1,25 @@
+package es.codeurjc.daw.library.service;
+
+import es.codeurjc.daw.library.model.Guide;
+import es.codeurjc.daw.library.repository.GuideRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GuideService {
+
+    private final GuideRepository guideRepository;
+
+    public GuideService(GuideRepository guideRepository) {
+        this.guideRepository = guideRepository;
+    }
+
+    public List<Guide> getGuidesByTour(Long tourId) {
+        return guideRepository.findByTourId(tourId);
+    }
+
+    public Guide save(Guide guide) {
+        return guideRepository.save(guide);
+    }
+}
