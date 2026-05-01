fa48c36e00a0ae4200b884ecda5ca16979b0986a
Javier004 <j.hernandezca.2023@alumnos.urjc.es>
2026-05-01 20:27:35 +0200

    controlador de errores

--- src/main/java/es/apexexpeditions/library/controller/CustomErrorController.java
+++ src/main/java/es/apexexpeditions/library/controller/CustomErrorController.java
@@ -11,8 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model; // used for isadminattempt flag
 // endregion
 
@@ -34,15 +33,7 @@ public class CustomErrorController implements ErrorController {
      * @param request current HTTP request containing error details
      * @return path to mustache template to be rendered
      */
-    @RequestMapping(value = "/error", method = {
-            RequestMethod.GET,
-            RequestMethod.POST,
-            RequestMethod.PUT,
-            RequestMethod.DELETE,
-            RequestMethod.PATCH,
-            RequestMethod.HEAD,
-            RequestMethod.OPTIONS
-    })
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         // retrieve HTTP status code (e.g., 404, 403, 500) from request attributes
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
