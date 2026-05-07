5605440fc05127db72dd08958d3a60e726135114
marioortiz-6 <m.ortizl.2023@alumnos.urjc.es>
2026-05-07 19:47:28 +0200

    Añadida configuración al Application.java para estructurar las respuestas paginadas de forma correcta

--- src/main/java/es/apexexpeditions/library/Application.java
+++ src/main/java/es/apexexpeditions/library/Application.java
@@ -2,8 +2,11 @@ package es.apexexpeditions.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(  
+	pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 
 	public static void main(String[] args) {
